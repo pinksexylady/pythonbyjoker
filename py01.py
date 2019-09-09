4d4935271db6bba877b5194c1e420c755a03887d
pinksexylady <55089279+pinksexylady@users.noreply.github.com>
2019-09-09 21:20:33 +0800

    Add files via upload

---
+++ py01.py
@@ -0,0 +1,74 @@
+ 1
+# c= float(input('请输入摄氏温度'))
+# f= 1.8 * c + 32
+# print('%.1f摄氏度 = %.1f华氏度' % (c,f))
+
+
+
+
+# 2
+# import math  
+# r = float(input('请输入半径：'))
+# h= float(input('请输入高：'))
+# a = r * r * π
+# v = a * h
+# print('底面积：%f'%a)
+# print('体积：%f'%v)
+
+
+
+
+
+# 3
+# f = float(input('请输入英尺数：'))
+# m= f/0.305
+# print ('%.1ff is %.4f m'%(f,m))
+
+
+
+
+
+
+
+
+
+
+#4.
+#M=float(input('输入水量'))
+#f=float(input('输入最终温度'))
+#i=float(input('输入开始温度'))
+#Q= M*(f-i)*4184
+#print('输出你所需要的能量%.1f'%Q)
+
+
+
+#5.
+#c=float(input('输入差额'))
+#n=float(input('输入年利率'))
+#l=c*(n/1200)
+#print('输出利息为%.6f'%l)
+
+#6.
+#v0=float(input('输入v0'))
+#v1=float(input('输入v1'))
+#t=float(input('输入时间t'))
+#a=(v1-v0)/t
+#print('输出平均加速度a%.4f'%a)
+
+#7.
+# money = float(input('请输入每个月存款数'))
+# a = money * ( 1 + 0.00417)
+# b = ( a + money) * ( 1 + 0.00417)
+# c = ( b + money) * ( 1 + 0.00417)
+# d = ( c + money) * ( 1 + 0.00417)
+# e = ( d + money) * ( 1 + 0.00417)
+# f = ( e + money) * ( 1 + 0.00417)
+# print('六个月后账户余额为',f)
+
+#8.
+#number = int (input('请输入0到1000的数字：'))
+# bai = number//100
+# shi = number//10%10
+# ge = number%10
+# sum_ = bai + shi + ge
+# print('the sum of the digits is:%d'%sum_)
